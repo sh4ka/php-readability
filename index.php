6238429ed0e7014157b507ee0cee442281c0ee43
Jesús Flores <jesus.flores@bq.com>
2015-02-17 14:49:17 +0100

    restrict output to json and allow https

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
  * PHP Readability
  *
  * @author mingcheng<i.feelinglucky#gmail.com>
- * @date   2011-02-17
+ * @author jesus flores< jesusfloressanjose@gmail.com>
+ * @date   2015-02-17
  * @link   http://www.gracecode.com/
  */
 
@@ -13,10 +14,9 @@ require 'common.inc.php';
 require 'lib/Readability.inc.php';
 
 $request_url = getRequestParam("url",  "");
-$output_type = strtolower(getRequestParam("type", "html"));
+$output_type = 'json';
 
-// 如果 URL 参数不正确，则跳转到首页
-if (!preg_match('/^http:\/\//i', $request_url) ||
+if ((!preg_match('/^http:\/\//i', $request_url) && !preg_match('/^https:\/\//i', $request_url)) ||
     !filter_var($request_url, FILTER_VALIDATE_URL, FILTER_FLAG_SCHEME_REQUIRED)) {
     include 'template/index.html';
     exit;
@@ -25,8 +25,7 @@ if (!preg_match('/^http:\/\//i', $request_url) ||
 $request_url_hash = md5($request_url);
 $request_url_cache_file = sprintf(DIR_CACHE."/%s.url", $request_url_hash);
 
-// 缓存请求数据，避免重复请求
-if (file_exists($request_url_cache_file) && 
+if (file_exists($request_url_cache_file) &&
         (time() - filemtime($request_url_cache_file) < CACHE_TIME)) {
 
     $source = file_get_contents($request_url_cache_file);
@@ -50,31 +49,14 @@ if (file_exists($request_url_cache_file) &&
     @file_put_contents($request_url_cache_file, $source);
 }
 
-// 判断编码
-//if (!$charset = mb_detect_encoding($source)) {
-//}
 preg_match("/charset=([\w|\-]+);?/", $source, $match);
 $charset = isset($match[1]) ? $match[1] : 'utf-8';
 
-/**
- * 获取 HTML 内容后，解析主体内容
- */
 $Readability = new Readability($source, $charset);
 $Data = $Readability->getContent();
 
-switch($output_type) {
-    case 'json':
-        header("Content-type: text/json;charset=utf-8");
-        $Data['url'] = $request_url;
-        echo json_encode($Data);
-        break;
-
-    case 'html': default:
-        header("Content-type: text/html;charset=utf-8");
-
-        $title   = $Data['title'];
-        $content = $Data['content'];
-
-        include 'template/reader.html';
-}
+// output data
+header("Content-type: text/json;charset=utf-8");
+$Data['url'] = $request_url;
+echo json_encode($Data);
 
